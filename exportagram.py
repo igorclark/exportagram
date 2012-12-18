d69386fc63979f4368af4ae4e048492f33088047
igor+github.com@igorsjunkmail.com <igor@Igor-Clarks-iMac.local>
2012-12-18 23:54:54 +0000

    tabs->4spcs

--- exportagram.py
+++ exportagram.py
@@ -12,90 +12,90 @@ script, dl_dir, user_id, access_token = argv
 url = "https://api.instagram.com/v1/users/%s/media/recent/?access_token=%s&count=100" % (user_id, access_token)
 
 if not exists(dl_dir):
-	makedirs(dl_dir)
+    makedirs(dl_dir)
 
 # make sure credentials work
 try:
-	response = urllib2.urlopen(url)
+    response = urllib2.urlopen(url)
 except urllib2.HTTPError:
-	print "HTTP error. Is the user_id and access_token correct?"
-	exit(1)
+    print "HTTP error. Is the user_id and access_token correct?"
+    exit(1)
 
 data = simplejson.loads(response.read())
 next_max_id = data['pagination']['next_max_id']
 
 while True:
-	for image in data['data']:
-		if image['caption']:
-			caption = image['caption']['text']
-		else:
-			caption = ''
+    for image in data['data']:
+        if image['caption']:
+            caption = image['caption']['text']
+        else:
+            caption = ''
 
-		location = dict()
-		if image['location']:
-			for el in ['latitude', 'longitude', 'name']:
-				if image['location'].__contains__(el):
-					location[el] = image['location'][el]
-			
-		metadata = {
-			'id': image['id'],
-			'caption': caption,
-			'created_time': image['created_time'],
-			'tags': image['tags'],
-			'location': location,
-			'images': [
-				{'type': 'lo', 'url': image['images']['low_resolution']['url']},
-				{'type': 'hi', 'url': image['images']['standard_resolution']['url']},
-				{'type': 'th', 'url': image['images']['thumbnail']['url']}
-			],
-		}
-		print "%s - %s" % (metadata['id'], metadata['caption'])
+        location = dict()
+        if image['location']:
+            for el in ['latitude', 'longitude', 'name']:
+                if image['location'].__contains__(el):
+                    location[el] = image['location'][el]
+            
+        metadata = {
+            'id': image['id'],
+            'caption': caption,
+            'created_time': image['created_time'],
+            'tags': image['tags'],
+            'location': location,
+            'images': [
+                {'type': 'lo', 'url': image['images']['low_resolution']['url']},
+                {'type': 'hi', 'url': image['images']['standard_resolution']['url']},
+                {'type': 'th', 'url': image['images']['thumbnail']['url']}
+            ],
+        }
+        print "%s - %s" % (metadata['id'], metadata['caption'])
 
-		# lay out subdirectories in y/m/d & name individual folders with timestamp
-		ym = datetime.datetime.fromtimestamp(int(metadata['created_time'])).strftime('%Y' + sep + '%m' + sep + '%d')
-		hms = datetime.datetime.fromtimestamp(int(metadata['created_time'])).strftime('%H-%M-%S')
-		img_dir = dl_dir + sep + ym + sep + hms + '_' + metadata['id']
-		if not exists(img_dir):
-			makedirs(img_dir)
+        # lay out subdirectories in y/m/d & name individual folders with timestamp
+        ym = datetime.datetime.fromtimestamp(int(metadata['created_time'])).strftime('%Y' + sep + '%m' + sep + '%d')
+        hms = datetime.datetime.fromtimestamp(int(metadata['created_time'])).strftime('%H-%M-%S')
+        img_dir = dl_dir + sep + ym + sep + hms + '_' + metadata['id']
+        if not exists(img_dir):
+            makedirs(img_dir)
 
-		# write out data file
-		json_file_path = img_dir + sep + metadata['id'] + '_json.txt'
-		if not exists(json_file_path):
-			with open(json_file_path, "w") as json_file:
-				json_file.write(simplejson.dumps(metadata))
+        # write out data file
+        json_file_path = img_dir + sep + metadata['id'] + '_json.txt'
+        if not exists(json_file_path):
+            with open(json_file_path, "w") as json_file:
+                json_file.write(simplejson.dumps(metadata))
 
-		# work out img file type
-		for imgdata in metadata['images']:
-			img_file_path = img_dir + sep + metadata['id'] + '_' + imgdata['type']
-			match = re.compile(".*(?P<file_type>\.[a-z]+)$").match(imgdata['url'])
-			img_file_ext = None
-			if match:
-				img_file_ext = match.group('file_type')
-				if exists(img_file_path + img_file_ext):
-					continue
+        # work out img file type
+        for imgdata in metadata['images']:
+            img_file_path = img_dir + sep + metadata['id'] + '_' + imgdata['type']
+            match = re.compile(".*(?P<file_type>\.[a-z]+)$").match(imgdata['url'])
+            img_file_ext = None
+            if match:
+                img_file_ext = match.group('file_type')
+                if exists(img_file_path + img_file_ext):
+                    continue
 
-			opener = urllib2.build_opener()
-			opener.addheaders = [('User-agent', 'Mozilla/5.0')] # just in case
-			response = opener.open(imgdata['url'])
+            opener = urllib2.build_opener()
+            opener.addheaders = [('User-agent', 'Mozilla/5.0')] # just in case
+            response = opener.open(imgdata['url'])
 
-			if not img_file_ext:
-				img_file_ext = mimetypes.guess_extension(response.info()['Content-Type'])
-				if exists(img_file_path + img_file_ext):
-					continue
+            if not img_file_ext:
+                img_file_ext = mimetypes.guess_extension(response.info()['Content-Type'])
+                if exists(img_file_path + img_file_ext):
+                    continue
 
-			with open(img_file_path + img_file_ext, "wb") as img_file:
-				img_file.write(response.read())
+            with open(img_file_path + img_file_ext, "wb") as img_file:
+                img_file.write(response.read())
 
-	# got more pages to view?
-	if next_max_id:
-		new_url = url + "&max_id=" + next_max_id
-		response = urllib2.urlopen(new_url)
-		data = simplejson.loads(response.read())
-		if data['pagination'].__contains__('next_max_id'):
-			next_max_id = data['pagination']['next_max_id']
-		else:
-			next_max_id = None
-	# last retrieve was beginning of timeline, no more pagination
-	else:
-		break
+    # got more pages to view?
+    if next_max_id:
+        new_url = url + "&max_id=" + next_max_id
+        response = urllib2.urlopen(new_url)
+        data = simplejson.loads(response.read())
+        if data['pagination'].__contains__('next_max_id'):
+            next_max_id = data['pagination']['next_max_id']
+        else:
+            next_max_id = None
+    # last retrieve was beginning of timeline, no more pagination
+    else:
+        break
 
